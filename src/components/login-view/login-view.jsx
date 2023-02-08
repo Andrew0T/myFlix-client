602a88681d639ad45bfe2cbe72358eada7b30ac7
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-08 15:05:31 +0100

    Update login-view.jsx
    redefined import and token

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,24 +1,61 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { useState } from "react";
 
-export const LoginView = ({ user, onBackClick }) => {
-  return (
-    <div
-      onClick={() => {
-        onUserClick(user);
-      }}
-    >
-      {user.name}
-    </div>
-  );
-};
+export const LoginView = ({ onLoggedIn }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  LoginView.propTypes = {
-   User: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    birthday: PropTypes.string.isRequired
-  }).isRequired,
-onUserClick: PropTypes.func.isRequired
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+    
+    const data = {
+      Username: username,
+      Password: password
+    };
+
+    fetch("https://myflixdb-202302.herokuapp.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong.");
+      });
+
+    return (
+     <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+     </form>
+    );
+  };
 };
