ada7cca2db5541cf67acbbb097b6401a87ac7698
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-08 17:58:19 +0100

    Update movie-view.jsx
    corrections to capitals as in movie-card

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImagePath} />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie.title}</span>
+        <span>{movie.Title}</span>
       </div>
       <div>
         <span>Description: </span>
-        <span>{movie.description}</span>
+        <span>{movie.Description}</span>
       </div>
       <div>
         <span>Genre: </span>
-        <span>{movie.genre}</span>
+        <span>{movie.Genre}</span>
       </div>
       <div>
         <span>Director: </span>
