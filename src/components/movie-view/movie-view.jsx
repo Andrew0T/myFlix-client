b8197a2a4dbf5c5782676621606d0148f78cc65c
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-09 22:49:00 +0100

    Update movie-view.jsx
    import react and correct imagepath

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.imagepath} />
       </div>
       <div>
         <span>Title: </span>
