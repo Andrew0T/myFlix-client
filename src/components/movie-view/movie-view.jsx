9109c58f3cb97aa68a00636cf3ec8fde96a1c7e5
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-14 13:20:09 +0100

    Update movie-view.jsx
    import components and values defined

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies }) => {
+  const {movieId} = useParams();
+  const movie = movie.find((m) => m.id === movieId);
+
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img className="w-100" src={movie.image} />
       </div>
       <div>
         <span>Title: </span>
@@ -23,7 +28,9 @@ export const MovieView = ({ movie, onBackClick }) => {
         <span>Director: </span>
         <span>{movie.Director}</span>
       </div>
-      <button onClick={onBackClick}>Back</button>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
     </div>
   );
 };
