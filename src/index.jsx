085ccfe210b7f9b65267bef173d8bc7054f0fd77
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-14 13:15:23 +0100

    Update index.jsx
    import bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Container } from 'react-bootstrap';
 
 import { MainView } from './components/main-view/main-view';
-
-// Import statement to indicate that you need to bundle `./index.scss`
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 // Main component (will eventually use all the others)
 
-const MyFlixApplication = () => {
+const MyFlixApp = () => {
   return (
     < Container>
       <MainView />
@@ -16,9 +16,6 @@ const MyFlixApplication = () => {
   );
 };
 
-// Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
-
-// Tells React to render your app in the root DOM element
-root.render(<MyFlixApplication />);
+root.render(<MyFlixApp />);
