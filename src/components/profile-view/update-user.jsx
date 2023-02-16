f91301f415b08f462ee350b682fc2e99fefa30ce
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-16 08:24:07 +0100

    Create update-user.jsx
    create update-user

---
+++ src/components/profile-view/update-user.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { useState } from "react";
+import {Button, Form, Card } from "react-bootstrap";
+
+export const SignupView = () => {
+  const [username, setUsername] = useState ("");
+  const [password, setPassword] = useState ("");
+  const [email, setEmail] = useState ("");
+  const [birthday, setBirthday] = useState ("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch(`https://myflixdb-202302.herokuapp.com/users/${user.Username}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Update Profile</Card.Title>
+          <Form onSubmit={(e) => handleSubmit(e)}>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username: </Form.Label>
+                <Form.Control
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                  minLength="3"
+                  placeholder="Please enter a username"
+                />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password: </Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  minLength="8"
+                  placeholder="Your password must have 8 or more characters"
+                />
+            </Form.Group>
+            <Form.Group controlId="formEmail">
+              <Form.Label>Email: </Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail (e.target.value)}
+                  required
+                  placeholder="Please enter your email address"
+                />
+            </Form.Group>
+            <Form.Group controlId="formBirthday">
+              <Form.Label>Birthday: </Form.Label>
+                <Form.Control
+                  type="date"
+                  value={birthday}
+                  onChange={(e) => setBirthday (e.target.value)}
+                  required
+                  placeholder="Please enter your birth date"
+                />
+            </Form.Group>
+            <Button variant="warning" type="submit">
+            Save changes?
+            </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+};
