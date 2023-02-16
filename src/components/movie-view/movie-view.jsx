f0896c997765b82e37eaae4e6f73fbcec8899cd8
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-16 08:18:45 +0100

    Update movie-view.jsx
    import components

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,36 +1,58 @@
 import React from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./movie-view.scss";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = () => {
   const {movieId} = useParams();
-  const movie = movie.find((m) => m.id === movieId);
-
+  const movie = movies.find((m) => m.id === movieId);
+  
   return (
-    <div>
-      <div>
-        <img className="w-100" src={movie.image} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.description}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.genre}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director}</span>
-      </div>
+    <Container className="content">
+    <Row>
+      <Col>
+        <Card>
+          <div>
+            <img className="w-100" src={movie.ImagePath} />
+          </div>
+          <div>
+          <span>Title: </span>
+          <span>{movie.title}</span>
+          </div>
+          <div>
+          <span>Description: </span>
+          <span>{movie.description}</span>
+          </div>
+          <div>
+          <span>Genre: </span>
+          <span>{movie.genre}</span>
+          </div>
+          <div>
+          <span>Director: </span>
+          <span>{movie.Director}</span>
+          </div>
+        </Card>
+      </Col>
       <Link to={`/`}>
-        <button className="back-button">Back</button>
+        <button className="button-back primary">Back</button>
       </Link>
-    </div>
+      <Link>
+        <Button 
+          className="add-to-favorite"
+          onClick={addFavoriteMovie}
+          disabled={movieExists}
+        > Add to Favorites
+        </Button>
+        <Button 
+          className="remove-from-favorite"
+          variant="warning"
+          onClick={removeFavoriteMovie}
+          disabled={disableRemove}
+        > Remove from Favorites
+        </Button> 
+      </Link>  
+    </Row>
+  </Container>
   );
 };
