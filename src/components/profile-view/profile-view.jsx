edaf43d6bfe267a5d3f1f66fd9823664f0b9c4ff
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-19 19:16:13 +0100

    Update profile-view.jsx
    update code links

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 
-import { UpdateUser } from './update-user';
 import { UserInfo } from './user-info';
+import { UpdateUser } from './update-user';
+import { DeleteUser } from './delete-user';
 import { FavoriteMovies } from './favorite-movies';
 
-export const ProfileView = () => {
+export const ProfileView = ({}) => {
+  const [name, setUsername] = useState ("");
+  const [password, setPassword] = useState ("");
+  const [email, setEmail] = useState ("");
+  const [birthday, setBirthday] = useState ("");
+  const [favoriteMovies, setFavoriteMovies] = useState ("");
+
+  const data = {
+    Username: username,
+    Password: password,
+    Email: email,
+    Birthday: birthday
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    }
+  const handleUpdate = (e) => {
+    e.preventDefault();
+   }
+
+  fetch(`https://myflixdb-202302.herokuapp.com/users/:Username`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then((data) => {
+          setUsername(name);
+          setPassword(password);
+          setEmail(email);
+          setBirthday(birthday);
+          setFavoriteMovies(favoriteMovies);
+        })
+        .then((response) => {
+          if (response.ok) {
+            alert("Update successful");
+            window.location.reload();
+          } else {
+            alert("Update failed");
+          }
+        });
 
   return (
     <Row>
-      <UserInfo name={user.Username} email={ user.Email} birthday={user.Birthday}/>
-      <UpdateUser handleUpdate={handleUpdate} handleSubmit={handleSubmit} />
+      <UserInfo name={user.Username} email={user.Email}/>
+      <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
+      <DeleteUser handleUpdate={handleUpdate}/>
       <FavoriteMovies favoriteMovies={FavoriteMovies} />
     </Row>
   );
