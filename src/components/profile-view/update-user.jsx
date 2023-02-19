916eeaa86c79bbe7e4e454f628c63b9f707c3e14
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-19 19:14:56 +0100

    Update update-user.jsx
    update code links

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,51 +1,40 @@
 import React from "react";
-import { useState } from "react";
-import {Button, Form, Card } from "react-bootstrap";
+import {Button, Card, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export const SignupView = () => {
-  const [username, setUsername] = useState ("");
-  const [password, setPassword] = useState ("");
-  const [email, setEmail] = useState ("");
-  const [birthday, setBirthday] = useState ("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
+export const UpdateUser = ({ handleUpdate }) => {
+  const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [birthday, setBirthday] = useState("");
+    
     const data = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    };
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday
+        };
 
-    fetch(`https://myflixdb-202302.herokuapp.com/users/${user.Username}`, {
-      method: "POST",
+        fetch(`https://myflixdb-202302.herokuapp.com/users/:Username`, {
+      method: "PUT",
+      body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Login response: ", data);
-        if (data.user) {
-          localStorage.setItem("user", JSON.stringify(data.user));
-          localStorage.setItem("token", data.token);
-          onLoggedIn(data.user, data.token);
-        } else {
-          alert("No such user");
-        }
-      })
-      .catch((e) => {
-        alert("Something went wrong");
-      });
-  };
-
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert("Update successful");
+        window.location.reload();
+      } else {
+        alert("Sorry something is incorrect?");
+      }
+    });
+  
   return (
     <Card>
       <Card.Body>
         <Card.Title>Update Profile</Card.Title>
-          <Form onSubmit={(e) => handleSubmit(e)}>
+          <Form onSubmit={(e) => handleUpdate(e)}>
             <Form.Group controlId="formUsername">
               <Form.Label>Username: </Form.Label>
                 <Form.Control
@@ -73,7 +62,7 @@ export const SignupView = () => {
                 <Form.Control
                   type="email"
                   value={email}
-                  onChange={(e) => setEmail (e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                   placeholder="Please enter your email address"
                 />
@@ -83,14 +72,16 @@ export const SignupView = () => {
                 <Form.Control
                   type="date"
                   value={birthday}
-                  onChange={(e) => setBirthday (e.target.value)}
+                  onChange={(e) => setBirthday(e.target.value)}
                   required
                   placeholder="Please enter your birth date"
                 />
             </Form.Group>
+            <Link>
             <Button variant="warning" type="submit">
             Save changes?
             </Button>
+            </Link>
         </Form>
       </Card.Body>
     </Card>
