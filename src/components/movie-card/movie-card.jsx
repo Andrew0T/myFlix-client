bafbb07173d8f5c12b2efcc780280368b9fc3dfe
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-19 19:21:29 +0100

    Update movie-card.jsx
    updated code links and values

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -4,44 +4,27 @@ import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
+  
   return (
-    <Card className="h-100">
+    <Card classname="h-100">
       <Card.Img 
         variant="top"
         src={movie.ImagePath}
       />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
-        <Card.Text>{movie.Description}</Card.Text>
-        <Card.Title>{movie.Genre.Name}</Card.Title>
-        <Card.Text>{movie.Genre.Description}</Card.Text>
-        <Card.Title>{movie.Director.Name}</Card.Title>
-        <Card.Text>{movie.Director.Bio}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
-            <Button variant="secondary">Info</Button>
-          </Link>
-      </Card.Body>
+        {/* <Card.Text>{movie.Description}</Card.Text>
+        <Card.Text>{movie.Year}</Card.Text> */}
+        <Link to={`/movies`}>
+          <Button variant="primary">Info</Button>
+        </Link>
+      </Card.Body>      
     </Card>
   );
 };
 
-// Here is where we define all the props constraints for the BookCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }).isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Born: PropTypes.string.isRequired,
-      Death: PropTypes.string.isRequired
-    }).isRequired
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
 };
