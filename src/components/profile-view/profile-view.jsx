f9d5d6e42d057217460c1c956a20852beafa7f20
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-24 16:28:16 +0100

    Update profile-view.jsx
    update code

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Row } from 'react-bootstrap';
 
 import { UserInfo } from './user-info';
@@ -7,53 +7,12 @@ import { DeleteUser } from './delete-user';
 import { FavoriteMovies } from './favorite-movies';
 
 export const ProfileView = ({}) => {
-  const [name, setUsername] = useState ("");
-  const [password, setPassword] = useState ("");
-  const [email, setEmail] = useState ("");
-  const [birthday, setBirthday] = useState ("");
-  const [favoriteMovies, setFavoriteMovies] = useState("");
-
-  const data = {
-    Username: name,
-    Password: password,
-    Email: email,
-    Birthday: birthday
-};
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    }
-  const handleUpdate = (e) => {
-    e.preventDefault();
-  }
  
-  fetch(`https://myflixdb-202302.herokuapp.com/users/`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-        .then((data) => {
-          setUsername(name);
-          setPassword(password);
-          setEmail(email);
-          setBirthday(birthday);
-          setFavoriteMovies(favoriteMovies);
-        })
-        .then((response) => {
-          if (response.ok) {
-            alert("Update successful");
-            window.location.reload();
-          } else {
-            alert("Update failed");
-          }
-        });
-
   return (
     <Row>
-      <UserInfo name={user.Username} email={user.Email}/>
-      <UpdateUser handleSubmit={handleSubmit} handleUpdate={handleUpdate} />
-      <DeleteUser handleUpdate={handleUpdate}/>
+      <UserInfo username={UserInfo}/>
+      <UpdateUser handleUpdate={UpdateUser} />
+      <DeleteUser handleUpdate={DeleteUser}/>
       <FavoriteMovies favoriteMovies={FavoriteMovies} />
     </Row>
   );
