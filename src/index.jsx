1bfbac7d8f41eb0f83d0b6c922510e05c8960a5f
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-24 15:35:40 +0100

    Update index.jsx
    remove bootstrap/dist/css/bootstrap.min.css

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Container } from 'react-bootstrap';
 
 import { MainView } from './components/main-view/main-view';
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
 const MyFlixApp = () => {
