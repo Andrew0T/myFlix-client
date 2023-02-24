f64f8b00da26a1c69cb8f96eb807cfc9ef64a843
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-24 16:29:55 +0100

    Update update-user.jsx
    update connections

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,89 +1,92 @@
 import React from "react";
+import { useState } from "react";
 import {Button, Card, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
 
-export const UpdateUser = ({ handleUpdate }) => {
-  const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [birthday, setBirthday] = useState("");
-    
-    const data = {
-          Username: username,
-          Password: password,
-          Email: email,
-          Birthday: birthday
-        };
+export const UpdateUser = ({}) => {
+  const [username, setUsername] = useState ("");
+  const [password, setPassword] = useState ("");
+  const [email, setEmail] = useState ("");
+  const [birthday, setBirthday] = useState ("");
 
-        fetch(`https://myflixdb-202302.herokuapp.com/users/:Username`, {
+  const data = {
+    Username: username,
+    Password: password,
+    Email: email,
+    Birthday: birthday
+  };
+
+  const updateUser = (e) => {
+    e.preventDefault();
+
+    fetch(`https://myflixdb-202302.herokuapp.com/users/:Username`, {
       method: "PUT",
       body: JSON.stringify(data),
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        headers: { Authorization: `Bearer ${token}` }
       }
-    }).then((response) => {
-      if (response.ok) {
-        alert("Update successful");
-        window.location.reload();
-      } else {
-        alert("Sorry something is incorrect?");
-      }
-    });
-  
+    })
+    .then((response) => {
+      alert('Successfully updated user nformation');
+      return response.json(), console.log(response);
+  })
+  .catch((error) => {
+      alert('Sorry, something went wrong? Please try again!' + error);
+  });
+  };
+
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Update Profile</Card.Title>
-          <Form onSubmit={(e) => handleUpdate(e)}>
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  minLength="3"
-                  placeholder="Please enter a username"
-                />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password: </Form.Label>
-                <Form.Control
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  minLength="8"
-                  placeholder="Your password must have 8 or more characters"
-                />
-            </Form.Group>
-            <Form.Group controlId="formEmail">
-              <Form.Label>Email: </Form.Label>
-                <Form.Control
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                  placeholder="Please enter your email address"
-                />
-            </Form.Group>
-            <Form.Group controlId="formBirthday">
-              <Form.Label>Birthday: </Form.Label>
-                <Form.Control
-                  type="date"
-                  value={birthday}
-                  onChange={(e) => setBirthday(e.target.value)}
-                  required
-                  placeholder="Please enter your birth date"
-                />
-            </Form.Group>
-            <Link>
-            <Button variant="warning" type="submit">
-            Save changes?
-            </Button>
-            </Link>
-        </Form>
-      </Card.Body>
-    </Card>
+      <Card>
+        <Card.Body>
+          <Card.Title>Please Register</Card.Title>
+            <Form onSubmit={updateUser}>
+              <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                  <Form.Control
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    minLength="3"
+                    placeholder="Please enter a username"
+                  />
+              </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                  <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    minLength="6"
+                    placeholder="Your password must have 6 or more characters"
+                  />
+              </Form.Group>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                  <Form.Control
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="Please enter your email address"
+                  />
+              </Form.Group>
+              <Form.Group controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                  <Form.Control
+                    type="date"
+                    value={birthday}
+                    onChange={(e) => setBirthday(e.target.value)}
+                    required
+                    placeholder="Please enter your birth date"
+                  />
+              </Form.Group>
+              <Button variant="warning" type="submit">
+                Update user
+              </Button>
+          </Form>
+        </Card.Body>
+      </Card>
   );
 };
