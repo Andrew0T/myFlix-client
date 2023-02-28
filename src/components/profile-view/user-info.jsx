d1b28ae90be7362b04edde87f3d9d6e0f31d2bf2
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-28 14:14:07 +0100

    Update user-info.jsx
    change user info wording

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 
-export const UserInfo = ({ Username, Email }) => {
+export const UserInfo = ({ user, email }) => {
   
   return (
     <Row>
@@ -9,11 +9,11 @@ export const UserInfo = ({ Username, Email }) => {
         <h2>User Info</h2>  
         <div>
         <span>Username: </span>
-        <span>{Username}</span>
+        <span>{user}</span>
         </div>
         <div>
-        <span>Email: </span>/
-        <span>{Email}</span>
+        <span>Email: </span>
+        <span>{email}</span>
         </div>
       </Col>
     </Row>
