52378778de027e4ff26502199d7fbab01f9501cc
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-28 14:08:31 +0100

    Update director-view.jsx
    error corrections

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -3,57 +3,59 @@ import PropTypes from "prop-types";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-export const DirectorView = ({ onBackClick }) => {
+export const DirectorView = ({ movie, director, onBackClick }) => {
 
   return (
     <Container className="content">
-    <Row>
-      <Col>
-      <Card.Header>
-        {director.Name}
-        </Card.Header>
-      <Card.Body className="director-textarea">
-        <Card>
-          <div>
-          <span>Name: </span>
-          <span>{director.Name}</span>
-          </div>
-          <div>
-          <span>Biography: </span>
-          <span>{director.Bio}</span>
-          </div>
-          <div>
-          <span>Born: </span>
-          <span>{director.Birth}</span>
-          </div>
-          <div>
-          <span>Death: </span>
-          <span>{director.Death}</span>
-          </div>
-          <Link to={`/movies/directors`}>
-            <Button
-            className="button-director-view"
-            variant="secondary"
-            onClick={() => { onBackClick();
-            }}>
-            Back
-            </Button>
-          </Link>
-        </Card>
-        </Card.Body>
-      </Col>
-      
-    </Row>
-  </Container>
+      <Row>
+        <Col>
+          <Card.Header>{director}</Card.Header>
+            <Card>
+              <div>
+              <span>Name: </span>
+              <span>{director.Name}</span>
+              </div>
+              <div>
+              <span>Biography: </span>
+              <span>{director.Bio}</span>
+              </div>
+              <div>
+              <span>Born on: </span>
+              <span>{director.Birth}</span>
+              </div>
+              <div>
+              <span>Died on: </span>
+              <span>{director.Death}</span>
+              </div>
+              <Card.Body className="flex-column">
+              <Card.Title>{director}</Card.Title>
+              <Link to={`/movies/directors/${movie._id}`}>
+                <Button 
+                  variant="primary">
+                    Open
+                </Button>
+              </Link>
+              <Link to={`/movies/directors/${movie._id}`}>
+                <Button
+                variant="secondary"
+                onClick={() => { onBackClick();
+                }}>
+                Back
+                </Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
  
 DirectorView.propTypes = {
-  movie: PropTypes.array.isRequired,
   director: PropTypes.shape({
     Name: PropTypes.string.isRequired,
     Bio: PropTypes.string.isRequired,
     Birth: PropTypes.string.isRequired,
     Death: PropTypes.string.isRequired
-  }).isRequired,
+  }).isRequired
 };
