08d86dc3ae6604f7da15c88bc152075d821512a9
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-02-28 14:24:18 +0100

    Update favorite-movies.jsx
    container added,
    token and user defined
    error corrections

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
-import { Button, Col, Row,} from "react-bootstrap";
+import React, { useCallback } from "react";
+import { Button, Col, Container, Row,} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { MovieCard } from "../movie-card/movie-card";
 
-export const FavoriteMovies = ({ movies }) => {
-  const [user, setUser] = useState([]);
-  let favoriteMovies = movies.filter(movie => user.FavoriteMovies.includes(movie._id));
+export const FavoriteMovies = ({ movies, user, token }) => {
+  const favoriteMovies = useCallback(()=>{movies.filter((movie) => user.favoriteMovies.includes(movie.id))},[movies]);
 
   const addFavoriteMovie = () => {
-    fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movies/${movies}`, {        
+    if (!token) {
+      return;
+    }
+    fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movies/${movies.id}`, {        
             method: 'POST',
             headers: { 
               "Content-Type": "application/json",
@@ -24,61 +26,67 @@ export const FavoriteMovies = ({ movies }) => {
         })
     };
 
-const deleteMovie = () => {
-  fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movies/${movies}`, {
-          method: 'DELETE',
-          headers: { 
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}` }
-      })
-      .then((response) => {
-          alert('Movie has been deleted');
-          return response.json(), console.log(response);
-      })
-      .catch((error) => {
-          alert('Something went wrong' + error);
-      })
-    };
+  const deleteMovie = () => {
+    if (!token) {
+      return;
+    }
+    fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movies/${movies.id}`, {
+            method: 'DELETE',
+            headers: { 
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}` }
+        })
+        .then((response) => {
+            alert('Movie has been deleted');
+            return response.json(), console.log(response);
+        })
+        .catch((error) => {
+            alert('Something went wrong' + error);
+        })
+      };
 
   return (
-    <Row>
-      {favoriteMovies.length === 0 ? (
-        <Col>Your list of favorite movies is empty</Col>
-      ) : (
-        <>
-          <div className='text-start h2 mb-4'>Your list of favorite movies</div>
-          {favoriteMovies.map((movie) => (
-            <Col className='mb-5' key={movie.id} xs={12} sm={6} md={4} lg={3}>
-              <MovieCard
-                keys={movie._id}
-                movie={movie}
-                user={user}
-                updateUserOnFav={(user) => {
-                  console.log('Update User called', user);
-                  setUser(user);
-                  localStorage.setItem('user', JSON.stringify(user));
-                }}
-              />
-            </Col>
-          ))}
-        </>
-      )}
-      <Link to={`/movies/${movies._id}`}>
-        <Button 
-          className="add-to-favorite"
-          variant="warning"
-          onClick={addFavoriteMovie}
-        > Add to Favorites
-        </Button>
-        </Link>
-        <Link to={`/movies/${movies._id}`}>
-        <Button 
-          className="remove-from-favorite"
-          variant="danger"
-          onClick={deleteMovie}
-        > Remove from Favorites
-        </Button>
-        </Link>
-    </Row>
+    <Container className="content">
+        <Row>
+          {favoriteMovies.length === 0 ? (
+            <Col>Your list of favorite movies is empty</Col>
+          ) : (
+            <>
+              <div className='text-start h2 mb-4'>Your list of favorite movies</div>
+              {favoriteMovies.map((movie) => (
+                <Col key={movie._id} className='mb-5' xs={12} sm={6} md={4} lg={3}>
+                  <MovieCard
+                    movie={movie}
+                    user={user}
+                    token={token}
+                    updateUserOnFav={(user) => {
+                      console.log('Update User called', user);
+                      setUser(user);
+                      setToken(token);
+                      localStorage.setItem('user', JSON.stringify(user));
+                    }}
+                  />
+                </Col>
+              ))}
+            </>
+          )}
+          <Link to={`/movies`}>
+            <Button 
+              className="add-to-favorite"
+              variant="warning"
+              onClick={addFavoriteMovie}
+            > Add to Favorites
+            </Button>
+          </Link>
+          <Link to={`/movies`}>
+            <Button 
+              className="remove-from-favorite"
+              variant="danger"
+              onClick={deleteMovie}
+            > Remove from Favorites
+            </Button>
+          </Link>
+        </Row>
+    </Container>
   );
 };
