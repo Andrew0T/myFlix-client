ed93a709ae2447ddaa2aaa2f3ebdab007035e6b2
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-03-10 08:57:29 +0100

    Update movie-card.jsx
    cleaning code and error corrections

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,29 +1,46 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const MovieCard = ({ movie }) => {
   
   return (
-    <Container className="content">
-      <Row>
-        <Col>
-          <Card className="h-100">
-            <Card.Img 
-              variant="top"
-              src={movie.ImagePath}
-            />
-            <Card.Body className="flex-column">
-              <Card.Title>{movie.Title}</Card.Title>
-              <Link to={`/movies/${movie._id}`}>
-                <Button variant="primary">Open</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <Card className="h-100" >
+      <Card.Img 
+        variant="top"
+        src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <br /> Director: 
+        <Link 
+          to={`/movies/${movie._id}/Director`}
+        >
+        <Button
+          variant="link">
+          {movie.Director.Name}
+        </Button>
+        </Link>
+        <br />Genre: 
+        <Link 
+          to={`/movies/${movie._id}/Genre`}
+        >
+        <Button variant="link">
+          {movie.Genre.Name}
+        </Button>
+        </Link>
+        <br />
+        <Link 
+          to={`/movies/${movie._id}`}
+        >
+        <Button 
+          variant="primary"
+        >
+          Open
+        </Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
@@ -32,6 +49,16 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
     Description: PropTypes.string,
-    Year: PropTypes.string    
+    Year: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }).isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    }).isRequired
   }).isRequired
 };
