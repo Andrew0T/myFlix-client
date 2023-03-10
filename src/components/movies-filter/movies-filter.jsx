2315709b7e5fb38f7dc9c06569c775bbfed7cdb6
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-03-10 08:59:57 +0100

    Update movies-filter.jsx
    code error corrections

--- src/components/movies-filter/movies-filter.jsx
+++ src/components/movies-filter/movies-filter.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Form from "react-bootstrap/Form";
 import { setFilter } from "../../redux/reducers/movies";
