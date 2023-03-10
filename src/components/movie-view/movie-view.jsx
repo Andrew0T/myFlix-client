797c0d6a98a1a478e2eaf792d35001561c796b8c
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-03-10 08:57:53 +0100

    Update movie-view.jsx
    cleaning code and error corrections

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,23 +1,23 @@
-import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import "./movie-view.scss";
 
 export const MovieView = ({ user, token }) => {
   const movies = useSelector((state) => state.movies.list);
-  const { movieId } = useParams();
-  const movie = movies.find((movie) => movie._id === movieId);
+  const { movie_Id } = useParams();
+  const movie = movies.find((movie) => movie._id === movie_Id);
 
   const addFavoriteMovie = () => {
     if (!token) {
       return;
     }
-    fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movies/${movie._id}`, {
+    fetch(`https://myflixdb-202302.herokuapp.com/users/${user}/movie/${movies._id}`, {        
             method: 'POST',
-            headers: {
+            headers: { 
               "Content-Type": "application/json",
-              Authorization: `Bearer ${token}` },
+              Authorization: `Bearer ${token}` 
+            }
         })
         .then((response) => {
             alert('Movie has been added to Favorite Movies');
@@ -26,42 +26,51 @@ export const MovieView = ({ user, token }) => {
         .catch((error) => {
             alert('Something went wrong' + error);
         });
-      };
+    };
 
   return (
-    <Container className="content">
-      <Row>
-        <Col>
-          <Card>
-            <div>
-              <img className="w-100" src={movie.ImagePath} />
-            </div>
-            <div>
-            <span>Title: </span>
-            <span>{movie.Title}</span>
-            </div>
-            <div>
-            <span>Description: </span>
-            <span>{movie.Description}</span>
-            </div>
-            <div>
-              <span>Year: </span>
-              <span>{movie.Year}</span>
-            </div>
-            <Link to={`/`}>
-              <button className="button-back">Back</button>
-            </Link>
-            <Link to={`/movies/${movie._id}`}>
-              <Button
-                className="add-to-favorite"
-                variant="warning"
-                onClick={addFavoriteMovie}
-              > Add to Favorites
-              </Button>
-            </Link>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+    <div>
+      <div>
+        <img className="w-50" src={movie.ImagePath} />
+      </div>
+      <div>
+      <span>Title: </span>
+      <span>{movie.Title}</span>
+      </div>
+      <div>
+      <span>Description: </span>
+      <span>{movie.Description}</span>
+      </div>
+      <div>
+        <span>Year: </span>
+        <span>{movie.Year}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.Director.Name}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.Genre.Name}</span>
+      </div>
+      <Link to={`/`}>
+        <Button
+          className="button-back"
+          style={{ cursor: "pointer" }}
+          type="reset"
+        >
+          Back
+        </Button>
+      </Link>
+      <Link to={`/users/:Username/movie/${movies._id}`}>
+            <Button 
+              className="add-to-favorite"
+              variant="warning"
+              onClick={addFavoriteMovie}
+            > 
+            Add to Favorites
+            </Button>
+          </Link>
+    </div>        
   );
 };
