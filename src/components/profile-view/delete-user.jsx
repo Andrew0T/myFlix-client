7042ab5b9e8a297bc9f615f88ba6a4afec86ed77
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-05-08 17:40:16 +0200

    Update delete-user.jsx
    error corrections and code clean up

--- src/components/profile-view/delete-user.jsx
+++ src/components/profile-view/delete-user.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
-import { Button, Card, Form } from "react-bootstrap";
+import React from "react";
+import { Button, Col } from "react-bootstrap";
 
 export const DeleteUser = () => {
   const token = localStorage.getItem("token");
   const user= JSON.parse(localStorage.getItem("user"));
-  
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   
   const deleteUser = (e) => {
     e.preventDefault();
@@ -18,53 +15,29 @@ export const DeleteUser = () => {
         Authorization: `Bearer ${token}` 
       },
     })
-    .then((response) => response.json())
     .then((resJSON) => {
-      alert('User account was successful deleted. Sorry to see you go.');
-      console.log(resJSON);
-      window.location.assign("/");
+        console.log(resJSON)      
+        alert('User account was successful deleted. Sorry to see you go.');
+        localStorage.clear("token, user");
+        window.location.assign("/");
     })
     .catch((error) => {
-      alert('Sorry, something went wrong' + error);
-    });
+      console.log(error);
+        alert('Sorry, something went wrong? Please try again!' + error);
+    })
   };
 
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Delete User</Card.Title>
-          <Form onSubmit={deleteUser}>
-            <Form.Group controlId="formUsername">
-              <Form.Label>Username: </Form.Label>
-                <Form.Control
-                  type="text"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  required
-                  minLength="6"
-                  placeholder="Please enter your username"
-                />
-            </Form.Group>
-            <Form.Group controlId="formPassword">
-              <Form.Label>Password: </Form.Label>
-                <Form.Control
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  minLength="6"
-                  placeholder="Please enter your password"
-                />
-            </Form.Group>
-            <Button              
-              variant="danger"
-              type="submit"
-              onClick={deleteUser}
-            >
-            Delete user?
-            </Button>
-          </Form>
-      </Card.Body>    
-    </Card>
+    <Col>
+      <div>
+        <Button              
+          variant="danger"
+          type="submit"
+          onClick={deleteUser}
+        >
+        Delete user?
+        </Button>
+      </div>
+    </Col>
   );
 };
