4ebd3af1766073ad1bcc8aff8c07928d0a34463d
AndrewT <113891991+Andrew0T@users.noreply.github.com>
2023-05-12 14:23:51 +0200

    Update favorite-movies.jsx
    error corrections

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -1,42 +1,21 @@
 import React from "react";
-import { Col, Container, Row,} from "react-bootstrap";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
-import { MoviesFilter } from "../movies-filter/movies-filter";
+import { Container, Row,} from "react-bootstrap";
+import { MoviesList } from "../movies-list/movies-list";
 
 export const FavoriteMovies = ({ movies }) => {
-  const token = localStorage.getItem("token");
   const user= JSON.parse(localStorage.getItem("user"));
-  let favoriteMovies = movies.filter(movie => user.favoriteMovies.includes(movie._id));
+  console.log(movies)
+  let favoriteMovies = movies.filter((movie) => user.FavoriteMovies.includes(movie._id));
+  const isFavoriteMovies = true;
 
   return (
     <Container className="content">
-        <Row>
-          <MoviesFilter />
-        </Row>
-        <Row>
-          <MovieView />
-        </Row>
-        <Row>
-          {movies.length === 0 ? (
-            <Col>
-            Your list of favorite movies is empty
-            </Col>
-          ) : (
-            <>
-              <Col className='text-start h2 mb-4'>
-              Your list of favorite movies
-              </Col>
-              {favoriteMovies.map(movie => (
-                <Col key={movie._id} className='mb-5' xs={12} sm={6} md={4} lg={3}>
-                  <MovieCard
-                    movie={movie}
-                  />
-                </Col>
-              ))}
-            </>
-          )}
-        </Row>
+      <Row>
+        <MoviesList 
+          isFavoriteMovies={isFavoriteMovies}
+          favoriteMovies= {favoriteMovies}
+        />
+      </Row>
     </Container>
   );
 };
